ee7081f5a8b688c777ff31a0a63b184ffe5196aa
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-09 20:02:11 +0900

    회원가입 테스트 코드 추가

--- src/test/java/com/yoo/core/CoreApplicationTests.java
+++ src/test/java/com/yoo/core/CoreApplicationTests.java
@@ -1,10 +1,25 @@
 package com.yoo.core;
 
+import com.yoo.core.member.*;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CoreApplicationTests {
+    MemberService memberService = new MemberServiceImpl();
+    @Test
+    void join() {
+        //given
+        Member member = new Member(1, "spring", Grade.VIP);
+
+        //when
+        memberService.join(member);
+        Member findMember = memberService.findMember(1);
+
+        //then
+        Assertions.assertThat(findMember).isEqualTo(member);
+    }
 
 
 
