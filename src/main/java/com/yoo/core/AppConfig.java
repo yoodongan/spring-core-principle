887fc29ef5e44c337a98d168938cf4f1d9143d27
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-10 22:45:33 +0900

    AppConfig 리팩토링, 정률할인정책으로 변경

--- src/main/java/com/yoo/core/AppConfig.java
+++ src/main/java/com/yoo/core/AppConfig.java
@@ -1,6 +1,8 @@
 package com.yoo.core;
 
-import com.yoo.core.discount.FixDiscountPolicy;
+import com.yoo.core.discount.DiscountPolicy;
+import com.yoo.core.discount.RateDiscountPolicy;
+import com.yoo.core.member.MemberRepository;
 import com.yoo.core.member.MemberService;
 import com.yoo.core.member.MemberServiceImpl;
 import com.yoo.core.member.MemoryMemberRepository;
@@ -9,11 +11,20 @@ import com.yoo.core.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
