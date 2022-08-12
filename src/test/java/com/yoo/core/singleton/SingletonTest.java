9c3ce5ed1c04afd37245c4ca3cc331b008b60a71
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-13 00:19:05 +0900

    스프링 없는 순순한 DI 컨테이너 테스트코드 작성

---
+++ src/test/java/com/yoo/core/singleton/SingletonTest.java
@@ -0,0 +1,24 @@
+package com.yoo.core.singleton;
+
+import com.yoo.core.AppConfig;
+import com.yoo.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너 테스트하기")
+    void singletonTest() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService2).isNotSameAs(memberService1);
+    }
+
+}
