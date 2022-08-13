fc278cb66ac79f29364656aae612bb9abb476763
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-13 10:00:11 +0900

    스프링컨테이러를 활용한 싱글톤 테스트

--- src/test/java/com/yoo/core/singleton/SingletonTest.java
+++ src/test/java/com/yoo/core/singleton/SingletonTest.java
@@ -5,6 +5,8 @@ import com.yoo.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class SingletonTest {
 
@@ -20,5 +22,19 @@ public class SingletonTest {
 
         Assertions.assertThat(memberService2).isNotSameAs(memberService1);
     }
+    @Test
+    @DisplayName("AppConfig-스프링컨테이너(DI컨테이너) 를 활용한 싱글톤 테스트")
+    void singletonTestByContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
 
 }
