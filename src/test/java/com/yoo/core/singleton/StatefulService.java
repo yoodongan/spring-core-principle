bc0e23adde71449f68d3705b6404236b37ae15cf
ryudongan <90627763+yoodongan@users.noreply.github.com>
2022-08-13 10:51:25 +0900

    Singleton 방식의 문제점 해결 - 일반변수 생성해서 return 값 저장

--- src/test/java/com/yoo/core/singleton/StatefulService.java
+++ src/test/java/com/yoo/core/singleton/StatefulService.java
@@ -2,14 +2,13 @@ package com.yoo.core.singleton;
 
 // stateful 한 객체 생성, 이렇게 설계하면 뭐가 문제인지 알아보자.
 public class StatefulService {
-    private int price;
+//    private int price;
 
     public int order(String name, int price) {
         System.out.printf("%s가 %d만큼 주문했습니다.",name, price);
-        this.price = price;
-    }
-    public int getPrice() {
+//        this.price = price;
         return price;
     }
+
 
 }
--- src/test/java/com/yoo/core/singleton/StatefulServiceTest.java
+++ src/test/java/com/yoo/core/singleton/StatefulServiceTest.java
@@ -10,28 +10,40 @@ import org.springframework.context.annotation.Bean;
 import javax.swing.plaf.nimbus.State;
 
 public class StatefulServiceTest {
-    @Test
-    @DisplayName("StatefulService 싱글톤 테스트")
-    void test() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(StatefulTest.class);
-        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
-        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
-        statefulService1.order("Park", 10000);
-        statefulService2.order("Park", 20000);
-        // 사용자 주문금액 확인!
-        int price1 = statefulService1.getPrice();
-        int price2 = statefulService2.getPrice();
-
-        System.out.println(price1);
-        System.out.println(price2);
-        Assertions.assertThat(price1).isEqualTo(price2);   // 이렇게 설계하면 안된다.
-    }
-
-
     static class StatefulTest{
         @Bean
         public StatefulService statefulService() {
             return new StatefulService();
         }
     }
+//    @Test
+//    @DisplayName("StatefulService 싱글톤 테스트")
+//    void test() {
+//        ApplicationContext ac = new AnnotationConfigApplicationContext(StatefulTest.class);
+//        StatefulService statefulService1 = ac.getBean("statefulService", StatefulService.class);
+//        StatefulService statefulService2 = ac.getBean("statefulService", StatefulService.class);
+//        statefulService1.order("Park", 10000);
+//        statefulService2.order("Park", 20000);
+//        // 사용자 주문금액 확인!
+//        int price1 = statefulService1.getPrice();
+//        int price2 = statefulService2.getPrice();
+//
+//        System.out.println(price1);
+//        System.out.println(price2);
+//        Assertions.assertThat(price1).isEqualTo(price2);   // 이렇게 설계하면 안된다.
+//    }
+    @Test
+    @DisplayName("서로 공유하는 변수 개념을 삭제")
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(StatefulTest.class);
+        StatefulService stetefulService1 = ac.getBean(StatefulService.class);
+        StatefulService stetefulService2 = ac.getBean(StatefulService.class);
+
+        int priceA = stetefulService1.order("spring", 1000);
+        int priceB = stetefulService2.order("summer", 2000);
+
+        Assertions.assertThat(priceA).isNotEqualTo(priceB);
+    }
+
+
 }
